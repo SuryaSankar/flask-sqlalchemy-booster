7805441d14202ceb1acf6ed6c53172100dd92393
SuryaSankar <suryashankar.m@gmail.com>
2016-03-19 19:59:43 +0530

    added last method to query

--- flask_sqlalchemy_booster/query_booster.py
+++ flask_sqlalchemy_booster/query_booster.py
@@ -10,3 +10,6 @@ class QueryBooster(BaseQuery):
 
     def asc(self, attr='id'):
         return self.order_by(getattr(self.model_class, attr))
+
+    def last(self, *criterion, **kwargs):
+        return self.filter_by(**kwargs).filter(*criterion).desc().first()
