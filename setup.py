4abaa33f78047de931a201f45412014109ed516a
SuryaSankar <suryashankar.m@gmail.com>
2017-02-16 00:40:11 +0530

    bumping up toolspy version

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ from setuptools import setup
 
 setup(
     name='Flask-SQLAlchemy-Booster',
-    version='0.4.75',
+    version='0.4.76',
     description='Querying and JSON Response generation wrappers for Flask-SQLAlchemy',
     long_description='Allows querying on Model classes, supports several common query operations, allows JSONification of models and relations and provides a simple query language for dynamically fetching data',
     packages=['flask_sqlalchemy_booster'],
     include_package_data=True,
     install_requires=[
-        "toolspy>=0.2.9",
+        "toolspy>=0.2.11",
         "Flask>=0.10.1",
         "SQLAlchemy==1.0.9",
         "Flask-SQLAlchemy>=2.1",
