4e5adfc9539adb5b9465d09a46a6286d1abc8ef2
SuryaSankar <suryashankar.m@gmail.com>
2017-07-03 12:41:58 +0530

    adding for other methods also

--- flask_sqlalchemy_booster/crud_api_view.py
+++ flask_sqlalchemy_booster/crud_api_view.py
@@ -23,46 +23,50 @@ def construct_get_view_function(
         cache_timeout=None):
     def get(_id):
         # print "Entering get function for %s" % request.path
-        _id = _id.strip()
-        if _id.startswith('[') and _id.endswith(']'):
+        try:
+            _id = _id.strip()
+            if _id.startswith('[') and _id.endswith(']'):
+                if permitted_object_getter is not None:
+                    resources = [permitted_object_getter()]
+                    ids = [_id[1:-1]]
+                else:
+                    ids = [int(i) for i in json.loads(_id)]
+                    if get_query_creator:
+                        resources = get_query_creator(model_class.query).get_all(ids)
+                    else:
+                        resources = model_class.get_all(ids)
+                if None in resources:
+                    if all(r is None for r in resources):
+                        status = "failure"
+                    else:
+                        status = "partial_success"
+                else:
+                    status = "success"
+                return render_json_list_with_requested_structure(
+                    resources,
+                    pre_render_callback=lambda output_dict: {
+                        'status': status,
+                        'result': {
+                            _id: {'status': 'failure', 'error': 'Resource not found'}
+                            if obj is None
+                            else {'status': 'success', 'result': obj}
+                            for _id, obj in zip(ids, output_dict['result'])}
+                    },
+                    dict_struct=dict_struct
+                )
             if permitted_object_getter is not None:
-                resources = [permitted_object_getter()]
-                ids = [_id[1:-1]]
+                obj = permitted_object_getter()
             else:
-                ids = [int(i) for i in json.loads(_id)]
                 if get_query_creator:
-                    resources = get_query_creator(model_class.query).get_all(ids)
+                    obj = get_query_creator(model_class.query).get(_id)
                 else:
-                    resources = model_class.get_all(ids)
-            if None in resources:
-                if all(r is None for r in resources):
-                    status = "failure"
-                else:
-                    status = "partial_success"
-            else:
-                status = "success"
-            return render_json_list_with_requested_structure(
-                resources,
-                pre_render_callback=lambda output_dict: {
-                    'status': status,
-                    'result': {
-                        _id: {'status': 'failure', 'error': 'Resource not found'}
-                        if obj is None
-                        else {'status': 'success', 'result': obj}
-                        for _id, obj in zip(ids, output_dict['result'])}
-                },
-                dict_struct=dict_struct
-            )
-        if permitted_object_getter is not None:
-            obj = permitted_object_getter()
-        else:
-            if get_query_creator:
-                obj = get_query_creator(model_class.query).get(_id)
-            else:
-                obj = model_class.get(_id)
-        if obj is None:
-            return error_json(404, 'Resource not found')
-        return render_json_obj_with_requested_structure(obj, dict_struct=dict_struct)
+                    obj = model_class.get(_id)
+            if obj is None:
+                return error_json(404, 'Resource not found')
+            return render_json_obj_with_requested_structure(obj, dict_struct=dict_struct)
+
+        except Exception as e:
+            return error_json(400, e.message)
 
     if enable_caching and cache_handler is not None:
         cached_get = cache_handler.memoize(timeout=cache_timeout)(get)
@@ -75,12 +79,14 @@ def construct_index_view_function(
         enable_caching=False, cache_handler=None, cache_key_determiner=None,
         cache_timeout=None):
     def index():
-        # print "Entering index function for %s" % request.path
-        if callable(index_query_creator):
-            return process_args_and_render_json_list(
-                index_query_creator(model_class.query),
-                dict_struct=dict_struct)
-        return process_args_and_render_json_list(model_class, dict_struct=dict_struct)
+        try:
+            if callable(index_query_creator):
+                return process_args_and_render_json_list(
+                    index_query_creator(model_class.query),
+                    dict_struct=dict_struct)
+            return process_args_and_render_json_list(model_class, dict_struct=dict_struct)
+        except Exception as e:
+            return error_json(400, e.message)
 
     if enable_caching and cache_handler is not None:
         if cache_key_determiner is None:
@@ -280,116 +286,122 @@ def construct_batch_put_view_function(
         fields_forbidden_from_being_set=None):
 
     def batch_put():
-        if pre_processors is not None:
-            for processor in pre_processors:
-                if callable(processor):
-                    processor()
-        fields_to_be_removed = union([
-            fields_forbidden_from_being_set or [],
-            model_class._fields_forbidden_from_being_set_ or []])
-        if len(fields_to_be_removed) > 0:
-            for dict_item in g.json.values():
-                delete_dict_keys(dict_item, fields_to_be_removed)
-        output = {}
-        obj_ids = g.json.keys()
-        if type(model_class.primary_key().type)==sqltypes.Integer:
-            obj_ids = [int(obj_id) for obj_id in obj_ids]
-        if callable(query_constructor):
-            objs = query_constructor(model_class.query).get_all(obj_ids)
-        else:
-            objs = model_class.get_all(obj_ids)
-        existing_instances = dict(zip(obj_ids, objs))
-        all_success = True
-        any_success = False
-        polymorphic_field = schema.get('polymorphic_on')
-        input_data = model_class.pre_validation_adapter_for_mapped_collection(g.json, existing_instances)
-        if isinstance(input_data, Response):
-            return input_data
-        updated_objects = {}
-        for obj_id, put_data_for_obj in input_data.items():
-            output_key = obj_id
+        try:
+            if pre_processors is not None:
+                for processor in pre_processors:
+                    if callable(processor):
+                        processor()
+            fields_to_be_removed = union([
+                fields_forbidden_from_being_set or [],
+                model_class._fields_forbidden_from_being_set_ or []])
+            if len(fields_to_be_removed) > 0:
+                for dict_item in g.json.values():
+                    delete_dict_keys(dict_item, fields_to_be_removed)
+            output = {}
+            obj_ids = g.json.keys()
             if type(model_class.primary_key().type)==sqltypes.Integer:
-                output_key = int(obj_id)
-            existing_instance = existing_instances[output_key]
-            if existing_instance is None:
-                output[output_key] = {
-                    "status": "failure",
-                    "result": "Resource not found"
-                }
-                all_success = False
-                any_success = any_success or False
+                obj_ids = [int(obj_id) for obj_id in obj_ids]
+            if callable(query_constructor):
+                objs = query_constructor(model_class.query).get_all(obj_ids)
             else:
-                if polymorphic_field:
-                    if polymorphic_field not in put_data_for_obj:
-                        put_data_for_obj[polymorphic_field] = getattr(existing_instance, polymorphic_field)
-                is_valid, errors = validate_object(
-                    schema, put_data_for_obj, allow_required_fields_to_be_skipped=True,
-                    allow_unknown_fields=allow_unknown_fields,
-                    context={
-                        "existing_instance": existing_instance,
-                        "model_class": model_class
-                    }, schemas_registry=schemas_registry)
-                if is_valid:
-                    updated_object = existing_instance.update_without_commit(
-                        **put_data_for_obj)
-                    if post_processors is not None:
-                        for processor in post_processors:
-                            if callable(processor):
-                                processed_updated_object = processor(updated_object, put_data_for_obj)
-                                if processed_updated_object is not None:
-                                    updated_object = processed_updated_object
-                    updated_objects[updated_object.id] = updated_object
-                    output[output_key] = {
-                        "status": "success",
-                        "result": serializable_obj(
-                            updated_object,
-                            **_serializable_params(request.args))
-                    }
-                    all_success = all_success and True
-                    any_success = True
-                else:
+                objs = model_class.get_all(obj_ids)
+            existing_instances = dict(zip(obj_ids, objs))
+            all_success = True
+            any_success = False
+            polymorphic_field = schema.get('polymorphic_on')
+            input_data = model_class.pre_validation_adapter_for_mapped_collection(g.json, existing_instances)
+            if isinstance(input_data, Response):
+                return input_data
+            updated_objects = {}
+            for obj_id, put_data_for_obj in input_data.items():
+                output_key = obj_id
+                if type(model_class.primary_key().type)==sqltypes.Integer:
+                    output_key = int(obj_id)
+                existing_instance = existing_instances[output_key]
+                if existing_instance is None:
                     output[output_key] = {
                         "status": "failure",
-                        "error": errors
+                        "result": "Resource not found"
                     }
                     all_success = False
                     any_success = any_success or False
-        final_status = "success"
-        if not all_success:
-            if any_success:
-                final_status = "partial_success"
-            else:
-                final_status = "failure"
-        return as_json({
-            "status": final_status,
-            "result": output
-        }, wrap=False)
+                else:
+                    if polymorphic_field:
+                        if polymorphic_field not in put_data_for_obj:
+                            put_data_for_obj[polymorphic_field] = getattr(existing_instance, polymorphic_field)
+                    is_valid, errors = validate_object(
+                        schema, put_data_for_obj, allow_required_fields_to_be_skipped=True,
+                        allow_unknown_fields=allow_unknown_fields,
+                        context={
+                            "existing_instance": existing_instance,
+                            "model_class": model_class
+                        }, schemas_registry=schemas_registry)
+                    if is_valid:
+                        updated_object = existing_instance.update_without_commit(
+                            **put_data_for_obj)
+                        if post_processors is not None:
+                            for processor in post_processors:
+                                if callable(processor):
+                                    processed_updated_object = processor(updated_object, put_data_for_obj)
+                                    if processed_updated_object is not None:
+                                        updated_object = processed_updated_object
+                        updated_objects[updated_object.id] = updated_object
+                        output[output_key] = {
+                            "status": "success",
+                            "result": serializable_obj(
+                                updated_object,
+                                **_serializable_params(request.args))
+                        }
+                        all_success = all_success and True
+                        any_success = True
+                    else:
+                        output[output_key] = {
+                            "status": "failure",
+                            "error": errors
+                        }
+                        all_success = False
+                        any_success = any_success or False
+            final_status = "success"
+            if not all_success:
+                if any_success:
+                    final_status = "partial_success"
+                else:
+                    final_status = "failure"
+            return as_json({
+                "status": final_status,
+                "result": output
+            }, wrap=False)
+        except Exception as e:
+            return error_json(400, e.message)
     return batch_put
 
 
 def construct_patch_view_function(model_class, schema, pre_processors=None,
                                   query_constructor=None, schemas_registry=None):
     def patch(_id):
-        if pre_processors is not None:
-            for processor in pre_processors:
-                if callable(processor):
-                    processor()
-        if callable(query_constructor):
-            obj = query_constructor(model_class.query).get(_id)
-        else:
-            obj = model_class.get(_id)
-        polymorphic_field = schema.get('polymorphic_on')
-        if polymorphic_field:
-            if polymorphic_field not in g.json:
-                g.json[polymorphic_field] = getattr(obj, polymorphic_field)
-        is_valid, errors = validate_object(
-            schema, g.json, allow_required_fields_to_be_skipped=True,
-            context={"existing_instance": obj,
-                     "model_class": model_class},
-            schemas_registry=schemas_registry)
-        if not is_valid:
-            return error_json(400, errors)
-        return render_json_obj_with_requested_structure(obj.update(**g.json))
+        try:
+            if pre_processors is not None:
+                for processor in pre_processors:
+                    if callable(processor):
+                        processor()
+            if callable(query_constructor):
+                obj = query_constructor(model_class.query).get(_id)
+            else:
+                obj = model_class.get(_id)
+            polymorphic_field = schema.get('polymorphic_on')
+            if polymorphic_field:
+                if polymorphic_field not in g.json:
+                    g.json[polymorphic_field] = getattr(obj, polymorphic_field)
+            is_valid, errors = validate_object(
+                schema, g.json, allow_required_fields_to_be_skipped=True,
+                context={"existing_instance": obj,
+                         "model_class": model_class},
+                schemas_registry=schemas_registry)
+            if not is_valid:
+                return error_json(400, errors)
+            return render_json_obj_with_requested_structure(obj.update(**g.json))
+        except Exception as e:
+            return error_json(400, e.message)
 
     return patch
 
