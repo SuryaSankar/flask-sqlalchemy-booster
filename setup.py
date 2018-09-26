e9d8da3fa0523cc5bd15bb5c6bf89183e3c5fa68
Surya Sankar <suryashankar.m@gmail.com>
2018-09-26 12:44:02 +0530

    upgrading version

--- setup.py
+++ setup.py
@@ -1,23 +1,10 @@
-"""
-Flask-SQLAlchemy-Booster
-------------------------
-
-Adds further methods to Flask-SQLAlchemy by subclassing the
-Model class and adding
-
-    1. Class methods to enable easy querying
-
-    2. Methods to convert the model object to a JSON convertible
-    dictionary.
-
-"""
 
 from setuptools import setup
 
 setup(
     name='Flask-SQLAlchemy-Booster',
-    version='0.5.11',
-    description='Querying and JSON Response generation wrappers for Flask-SQLAlchemy',
+    version='0.5.12',
+    description='A booster package for flask and sqlalchemy',
     long_description='Allows querying on Model classes, supports several common query operations, allows JSONification of models and relations and provides a simple query language for dynamically fetching data',
     packages=['flask_sqlalchemy_booster'],
     include_package_data=True,
