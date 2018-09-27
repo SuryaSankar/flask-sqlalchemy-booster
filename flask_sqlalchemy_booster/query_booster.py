85857469b138e09919aaf9f954436783c9c15b10
Surya Sankar <suryashankar.m@gmail.com>
2018-09-27 14:03:28 +0530

    removing the model_class definition from query booster init since it caused error elsewhere due primary_entity not being defined at the time of call. Added a separate property instead

--- flask_sqlalchemy_booster/query_booster.py
+++ flask_sqlalchemy_booster/query_booster.py
@@ -5,19 +5,23 @@ class QueryBooster(BaseQuery):
 
     cls = None
 
-    def __init__(self, *args, **kwargs):
-        super(QueryBooster, self).__init__(*args, **kwargs)
-        self.model_class = self._primary_entity.mapper.class_
+    # def __init__(self, *args, **kwargs):
+    #     super(QueryBooster, self).__init__(*args, **kwargs)
+    #     self.model_class = self._primary_entity.mapper.class_
+
+    @property
+    def mapper_model_class(self):
+        return self._primary_entity.mapper.class_
 
     def desc(self, attr=None):
         if attr is None:
-            attr = self.model_class.primary_key_name()
-        return self.order_by(getattr(self.model_class, attr).desc())
+            attr = self.mapper_model_class.primary_key_name()
+        return self.order_by(getattr(self.mapper_model_class, attr).desc())
 
     def asc(self, attr=None):
         if attr is None:
-            attr = self.model_class.primary_key_name()
-        return self.order_by(getattr(self.model_class, attr))
+            attr = self.mapper_model_class.primary_key_name()
+        return self.order_by(getattr(self.mapper_model_class, attr))
 
     def last(self, *criterion, **kwargs):
         return self.filter_by(**kwargs).filter(*criterion).desc().first()
@@ -26,10 +30,10 @@ class QueryBooster(BaseQuery):
         if len(keyvals) == 0:
             return []
         if key is None:
-            key = self.model_class.primary_key_name()
+            key = self.mapper_model_class.primary_key_name()
         original_keyvals = keyvals
         keyvals_set = list(set(keyvals))
-        resultset = self.filter(getattr(self.model_class, key).in_(keyvals_set))
+        resultset = self.filter(getattr(self.mapper_model_class, key).in_(keyvals_set))
         key_result_mapping = {getattr(result, key): result for result in resultset.all()}
         return [key_result_mapping.get(kv) for kv in original_keyvals]
 
