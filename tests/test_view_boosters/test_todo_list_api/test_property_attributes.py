16167feab45f666d7765c637c21f1ec387a76db2
SuryaSankar <suryashankar.m@gmail.com>
2019-06-25 16:38:19 +0530

    removing print line

--- tests/test_view_boosters/test_todo_list_api/test_property_attributes.py
+++ tests/test_view_boosters/test_todo_list_api/test_property_attributes.py
@@ -3,7 +3,6 @@ def test_serialization_of_property_attr(todolist_with_users_tasks):
 		resp = client.jget('/users?name~=Donald')
 		assert resp['status'] == 'success'
 		assert resp['result'][0]['name'] == "Donald Duck"
-        print(resp['result'][0])
         assert resp['result'][0].get('first_name') == 'Donald'
 
 def test_if_put_ignores_property_attr(todolist_with_users_tasks):
