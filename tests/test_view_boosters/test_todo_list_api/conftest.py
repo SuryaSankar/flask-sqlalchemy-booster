bd1ef650a41183a9d03efd14c003fa2d7a442e48
SuryaSankar <suryashankar.m@gmail.com>
2019-07-20 14:03:59 +0530

    converting tabs to spaces

--- tests/test_view_boosters/test_todo_list_api/conftest.py
+++ tests/test_view_boosters/test_todo_list_api/conftest.py
@@ -4,25 +4,28 @@ from .todo_list_api.app import db, create_todolist_app, User, Task
 
 @pytest.fixture(scope="session")
 def todolist_app():
-	return create_todolist_app()
+    return create_todolist_app()
 
 @pytest.fixture(scope="session")
 def todolist_with_users_tasks(todolist_app):
     with  todolist_app.test_request_context():
-		User.create_all([{
-			"name": "Donald Duck",
-			"email": "duck@disney.com",
-			"gender": "male"
-		}, {
-			"name": "Tin Tin",
-			"email": "tintin@cn.com",
-			"gender": "male"
-		}])
-		Task.create_all([{
-			"title": "Swim",
-			"user_email": "duck@disney.com"
-		}, {
-			"title": "Solve Mysteries",
-			"user_email": "tintin@cn.com"
-		}])
-		return todolist_app
+        users = [{
+            "name": "Donald Duck",
+            "email": "duck@disney.com",
+            "gender": "male"
+        }, {
+            "name": "Tin Tin",
+            "email": "tintin@cn.com",
+            "gender": "male"
+        }]
+        User.create_all(users)
+        Task.create_all(
+            [{
+                "title": "Swim",
+                "user_email": "duck@disney.com"
+            }, {
+                "title": "Solve Mysteries",
+                "user_email": "tintin@cn.com"
+            }]
+        )
+        return todolist_app
